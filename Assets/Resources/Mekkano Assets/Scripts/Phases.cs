288893dc8615b6f4b5be1f26d39557a06d06f2b7
iRobsc <dkdree@hotmail.com>
2014-03-20 23:29:09 +0100

    added attacking phase and started working with it

---
+++ Assets/Resources/Mekkano Assets/Scripts/Phases.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Phases : MonoBehaviour {
+
+	public static int phase;
+
+	public static void selectPhase(int currentPhase){
+		phase = currentPhase;
+		TouchHandler.unitSelection = false;
+		TouchHandler.unitMovement = false;
+		TouchHandler.unitAttacking = false;
+		switch(currentPhase){
+			case 1: 			
+				TouchHandler.unitSelection = true;
+				TouchHandler.unitMovement = true;
+				break;
+			case 2:
+				TouchHandler.unitSelection = true;
+				TouchHandler.unitAttacking = true;
+				break;
+			case 3:
+				break;
+		}
+	}
+
+	/*public static Mousepicking mousePicking;
+	
+	public Phases(Camera camera, Player currentPlayer, InputManager inputManager, AssetManager assetManager, Node rootNode, Grid grid){
+		mousePicking = new Mousepicking(camera, currentPlayer, inputManager, assetManager, rootNode, grid);
+		mousePicking.initialize();
+	}
+	
+	public void selectPhase(String currentPhase){
+		if (currentPhase == "movePhase"){
+			mousePicking.unitSelection = true;
+			mousePicking.unitMovement = true;
+		}
+		else if (currentPhase == "attackPhase"){
+			
+		}
+		else if (currentPhase == "defensePhase"){
+			
+		}
+	}*/
+}
